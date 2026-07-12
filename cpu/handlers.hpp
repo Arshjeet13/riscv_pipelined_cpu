cba2b56c67e801d589e7dce175386f20b10c1003
Arshjeet <arshjeet.bajwa@gmail.com>
2026-07-12 11:06:08 +0000

    Fix missing registers parameter in jal() in namespace J

--- cpu/handlers.hpp
+++ cpu/handlers.hpp
@@ -86,7 +86,7 @@ namespace isa
     namespace J
     {
         uint32_t get_imm(uint32_t instruction);
-        void jal   (int rd, uint32_t imm, uint32_t& pc);
+        void jal   (int rd, uint32_t imm, uint32_t* registers, uint32_t& pc);
         void handle_instr(uint32_t instruction, uint32_t* registers, uint32_t& pc);
     }
 
