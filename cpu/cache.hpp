240dc7be7625b46c03dec23a4597f2033471b50c
Arshjeet <arshjeet.bajwa@gmail.com>
2026-07-14 11:02:06 +0000

    Add hit and miss count tracking fields and getter methods

--- cpu/cache.hpp
+++ cpu/cache.hpp
@@ -2,10 +2,14 @@
 #include <cstdint>
 #include "memory.hpp"
 
-class Cache {
+class Cache{
 public:
     Cache(Memory& memory);
-
+    uint64_t getHitCount();
+    uint64_t getMissCount();
+    
 private:
     Memory& memory;
+    uint64_t hit_count  {0};
+    uint64_t miss_count {0};
 };
