6169bed5fb4f613c0bbb16681f76240b5a75596f
Arshjeet <arshjeet.bajwa@gmail.com>
2026-07-16 12:21:57 +0000

    Change the order of instantiations of cpu, memory, cache

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "cpu/cpu.hpp"
+#include "cpu/memory.hpp"
+#include "cpu/cache.hpp"
 
 int main() {
-    CPU cpu;
-    cpu.reset();
-    cpu.loadProgram("tests/test1_arithmetic.hex");
+    Memory memory;
+    memory.loadProgram("tests/test1_arithmetic.hex");
+
+    DCache dcache(memory);
+    ICache icache(memory);
+
+    CPU cpu(memory, dcache, icache);
     cpu.run();
+
     return 0;
 }
