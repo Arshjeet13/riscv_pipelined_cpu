ff8a056b6eead00844beeedda7a91e0c8de1b036
Arshjeet <arshjeet.bajwa@gmail.com>
2026-08-08 10:32:58 +0000

    Add hit, miss, writeback cost variables and a stats print function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include "cpu/cache.hpp"
 #include <iostream>
 
+constexpr uint64_t HIT_COST = 1;
+constexpr uint64_t MISS_COST = 100;
+constexpr uint64_t WRITEBACK_COST = 99;
+
 void printCacheStats(const char* label, Cache& cache) {
     uint64_t hits = cache.getHitCount();
     uint64_t misses = cache.getMissCount();
@@ -11,6 +15,26 @@ void printCacheStats(const char* label, Cache& cache) {
 
     std::cout << label << ": hits=" << hits << " misses=" << misses
               << " hit_rate=" << hit_rate << "\n";
+}
+
+void printTotalCycleCost(ICache& icache, DCache& dcache) {
+    uint64_t icache_cycles = icache.getHitCount() * HIT_COST
+                            + icache.getMissCount() * MISS_COST;
+
+    uint64_t dcache_misses = dcache.getMissCount();
+    uint64_t dirty_misses = dcache.getDirtyMissCount();
+    uint64_t clean_misses = dcache_misses - dirty_misses;
+    uint64_t flushed_lines = dcache.getFlushedLineCount();
+
+    uint64_t dcache_cycles = dcache.getHitCount() * HIT_COST
+                            + clean_misses * MISS_COST
+                            + dirty_misses * (MISS_COST + WRITEBACK_COST)
+                            + flushed_lines * WRITEBACK_COST;
+
+    std::cout << "icache cycle cost: " << icache_cycles << "\n";
+    std::cout << "dcache cycle cost: " << dcache_cycles
+              << " (flush cost: " << flushed_lines * WRITEBACK_COST << ")\n";
+    std::cout << "total cycle cost: " << (icache_cycles + dcache_cycles) << "\n";
 }
 
 int main() {
@@ -26,6 +50,7 @@ int main() {
     printCacheStats("icache", icache);
     printCacheStats("dcache", dcache);
     std::cout << "dcache: dirty_misses=" << dcache.getDirtyMissCount() << "\n";
+    printTotalCycleCost(icache, dcache);
 
     return 0;
 }
